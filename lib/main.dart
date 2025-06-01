e05ca7e88521802ab9bf333332cdbbe5e5192470
jbpranov2204 <jbpranov@gmail.com>
2025-06-01 13:17:34 +0530

    made changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'routes/app_routes.dart';
-import 'bindings/initial_binding.dart';
 import 'bindings/app_bindings.dart';
 
 void main() {
@@ -10,12 +9,10 @@ void main() {
       title: 'Notes App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light().copyWith(
-        // Add your light theme customizations here
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         appBarTheme: const AppBarTheme(foregroundColor: Colors.black87),
       ),
       darkTheme: ThemeData.dark().copyWith(
-        // Add your dark theme customizations here
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.blue,
           brightness: Brightness.dark,
@@ -32,7 +29,7 @@ void main() {
         ),
         scaffoldBackgroundColor: Colors.grey[900],
       ),
-      themeMode: ThemeMode.system, // Default to system theme
+      themeMode: ThemeMode.system,
       initialBinding: AppBindings(),
       getPages: AppRoutes.routes,
       initialRoute: '/',
